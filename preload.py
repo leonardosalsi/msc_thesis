59638cfc753ea9a171f0a9d070cdf2a14683379e
Leonardo Salsi <leonardo.salsi@outlook.com>
2024-12-17 16:51:42 +0100

    Add dataset polling

--- preload.py
+++ preload.py
@@ -3,19 +3,19 @@ from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 # Tokenizer and Model nucleotide-transformer-v2-500m-multi-species
 AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-500m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/models/nt-500m", trust_remote_code=True)
-AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-500m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/datasets/nt-500m", trust_remote_code=True)
+AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-500m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/models/nt-500m", trust_remote_code=True)
 
 # Tokenizer and Model nucleotide-transformer-v2-250m-multi-species
-AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-250m-multi-species", cache_dir="./models/nt-250m", trust_remote_code=True)
-AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-250m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/datasets/nt-250m", trust_remote_code=True)
+AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-250m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/models/nt-250m", trust_remote_code=True)
+AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-250m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/models/nt-250m", trust_remote_code=True)
 
 # Tokenizer and Model nucleotide-transformer-v2-100m-multi-species
-AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-100m-multi-species", cache_dir="./models/nt-100m", trust_remote_code=True)
-AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-100m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/datasets/nt-100m", trust_remote_code=True)
+AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-100m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/models/nt-100m", trust_remote_code=True)
+AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-100m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/models/nt-100m", trust_remote_code=True)
 
 # Tokenizer and Model nucleotide-transformer-v2-50m-multi-species
 AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-50m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/models/nt-50m", trust_remote_code=True)
-AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-50m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/datasets/nt-50m", trust_remote_code=True)
+AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-50m-multi-species", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/models/nt-50m", trust_remote_code=True)
 
 # Dataset
-load_dataset("InstaDeepAI/multi_species_genomes", cache_dir="./datasets/multi-species-genomes", trust_remote_code=True)
+load_dataset("InstaDeepAI/multi_species_genomes", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/datasets/multi-species-genomes", trust_remote_code=True)
