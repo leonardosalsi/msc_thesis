e6ed781caeaa5a3c1be9f7aed4718418b04c7ee6
Leonardo Salsi <leonardo.salsi@outlook.com>
2024-12-18 18:22:25 +0100

    Add dataset polling

--- preload_datasets.py
+++ preload_datasets.py
@@ -3,4 +3,5 @@ from datasets import load_dataset
 # Dataset
 #load_dataset("InstaDeepAI/multi_species_genomes", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/datasets/multi_species_genomes", trust_remote_code=True)
 #load_dataset("InstaDeepAI/multi_species_genomes", cache_dir="/cluster/customapps/biomed/grlab/users/salsil/msc_thesis/datasets/multi_species_genomes", trust_remote_code=True)
-load_dataset("InstaDeepAI/multi_species_genomes", cache_dir="/cluster/project/grlab/tmp/petagraph/dataset", trust_remote_code=True)
+dataset = load_dataset("InstaDeepAI/multi_species_gednomes", cache_dir="/cluster/project/grlab/tmp/petagraph/dataset", trust_remote_code=True)
+print(dataset[0])
