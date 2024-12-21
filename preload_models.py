448c10112680e281317713170bafe53d2168397f
Leonardo Salsi <leonardo.salsi@outlook.com>
2024-12-21 14:41:07 +0100

    Add MMC evaluation

---
+++ preload_models.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+import config
+
+cache_dir = config.models_cache_dir
+
+# Tokenizer and Model nucleotide-transformer-v2-500m-multi-species
+AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-500m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
+AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-500m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
+
+# Tokenizer and Model nucleotide-transformer-v2-250m-multi-species
+AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-250m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
+AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-250m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
+
+# Tokenizer and Model nucleotide-transformer-v2-100m-multi-species
+AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-100m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
+AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-100m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
+
+# Tokenizer and Model nucleotide-transformer-v2-50m-multi-species
+AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-50m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
+AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-50m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
