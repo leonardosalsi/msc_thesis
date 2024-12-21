a47dae15eea22e284746988d1b9e6d67d21bcd95
Leonardo Salsi <leonardo.salsi@outlook.com>
2024-12-21 14:43:16 +0100

    Add MMC evaluation

--- preload_models.py
+++ preload_models.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForMaskedLM
 import config
 
 cache_dir = config.models_cache_dir
-
+print(cache_dir)
 # Tokenizer and Model nucleotide-transformer-v2-500m-multi-species
 AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-500m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
 AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-v2-500m-multi-species", cache_dir=cache_dir, trust_remote_code=True)
