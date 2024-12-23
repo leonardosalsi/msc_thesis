96f9ca32cb32ea8a690cb1fd15713e80eae5df8d
Leonardo Salsi <leonardo.salsi@outlook.com>
2024-12-23 12:23:06 +0100

    Add MMC evaluation

--- eval_mcc.sh
+++ eval_mcc.sh
@@ -2,16 +2,16 @@
 
 #SBATCH --job-name=evaluate-mcc
 #SBATCH --output=out-mcc.txt
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=4
 #SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=40G
 #SBATCH -p gpu
-#SBATCH --gres=gpu:0
+#SBATCH --gres=gpu:1
 
-#source ~/.bashrc
-#enable_modules
-#module load python scipy-stack gcc arrow
-#module list
+source ~/.bashrc
+enable_modules
+module load python scipy-stack gcc arrow
+module list
 
 VENV=/cluster/home/salsil/gpu_venv
 source $VENV/bin/activate
