d710803158fd22b04b3753f6f4b34400a09e3e75
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-01-12 14:50:05 +0100

    Split multi genome dataset sequences into smaller bits Reason: NT-50M-v2 can only take 2048 tokens at once, applying overlapping tokenization would result in a too big number of tokens. Pre-Processing the data like this makes it easier to generate tokens that are not bigger in number than 2048 (rest is padded, like in the pretrained version)

--- create_1k_dataset.sh
+++ create_1k_dataset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sbatch jobs/create1k_ds/train_dataset.sh
-sbatch jobs/create1k_ds/test_dataset.sh
-sbatch jobs/create1k_ds/validation_dataset.sh
+sbatch jobs/create_1k_ds/train_dataset.sh
+sbatch jobs/create_1k_ds/test_dataset.sh
+sbatch jobs/create_1k_ds/validation_dataset.sh
