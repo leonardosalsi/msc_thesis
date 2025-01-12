2d12b3e4fb13475666f33f43c272330b63d63dee
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-01-12 14:45:41 +0100

    Split multi genome dataset sequences into smaller bits Reason: NT-50M-v2 can only take 2048 tokens at once, applying overlapping tokenization would result in a too big number of tokens. Pre-Processing the data like this makes it easier to generate tokens that are not bigger in number than 2048 (rest is padded, like in the pretrained version)

---
+++ jobs/create_1k_ds/validation_dataset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=multi_species_validation_1k
+#SBATCH --output=out/multi_species_validation_1k.txt
+#SBATCH --cpus-per-task=2
+#SBATCH --time=20:00:00
+#SBATCH --mem-per-cpu=2G
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+
+source ~/.bashrc
+conda activate gpu_env
+
+HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/generate1k.py validation
