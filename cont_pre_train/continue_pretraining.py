b156b34590efa251d5868bbd8fb65936cd229640
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-01-13 12:10:22 +0100

    Tokenize 1k-data for later use

--- cont_pre_train/continue_pretraining.py
+++ cont_pre_train/continue_pretraining.py
@@ -2,6 +2,7 @@
 
 import os
 import random
+from pprint import pprint
 from typing import List, Dict
 
 import torch
@@ -28,9 +29,9 @@ if __name__ == "__main__":
     test__path = os.path.join(dataset_path, "test")
     validation_path = os.path.join(dataset_path, "validation")
 
-    multi_species_genomes_train = load_from_disk(train__path).select(range(1000))
-    multi_species_genomes_test = load_from_disk(test__path)
-    multi_species_genomes_validation = load_from_disk(validation_path)
+    multi_species_genomes_train = load_from_disk(train__path).select(range(5000))
+    multi_species_genomes_test = load_from_disk(test__path).select(range(5000))
+    multi_species_genomes_validation = load_from_disk(validation_path).select(range(5000))
 
     model = AutoModelForMaskedLM.from_pretrained(
         "InstaDeepAI/nucleotide-transformer-v2-50m-multi-species",
@@ -39,59 +40,51 @@ if __name__ == "__main__":
         local_files_only=True
     )
 
-    print("HERE")
     tokenizer_batch_size = 2048
     tokenizer = OverlappingEsmTokenizer.from_pretrained(
         "/shared/models",
         model_max_length=2048,
     )
 
-    print(type(tokenizer).__name__)
     def tokenize_function(examples):
         outputs = tokenizer(examples["sequence"])
         return outputs
 
-
     tokenizer_name = type(tokenizer).__name__
-    print("HERE")
+    tokenizer_path = os.path.join(dataset_path, "tokenized", tokenizer_name)
+    pprint(tokenizer)
     dataset_train = multi_species_genomes_train.map(
         tokenize_function,
         batched=True,
         batch_size=tokenizer_batch_size,
         num_proc=os.cpu_count(),
-        cache_file_name=os.path.join("/shared/datasets/InstaDeepAI___multi_species_genomes/1kbp/tokenized/OverlappingEsmTokenizer/train/", f"{tokenizer_name}.arrow")
+        cache_file_name=os.path.join(tokenizer_path, "train", f"{tokenizer_name}.arrow"),
+        remove_columns=["sequence", "description", "start_pos", "end_pos"],
     )
-    print("HERE")
-    print(dataset_train)
-    exit(0)
     dataset_validation = multi_species_genomes_validation.map(
         tokenize_function,
         batched=True,
         batch_size=tokenizer_batch_size,
         num_proc=os.cpu_count(),
-        cache_file_name="test_overlap_tokenized"
+        cache_file_name=os.path.join(tokenizer_path, "validation", f"{tokenizer_name}.arrow"),
+        remove_columns=["sequence", "description", "start_pos", "end_pos"],
     )
 
     dataset_test = multi_species_genomes_test.map(
         tokenize_function,
         batched=True,
         batch_size=tokenizer_batch_size,
-        num_proc=os.cpu_count()
+        num_proc=os.cpu_count(),
+        cache_file_name=os.path.join(tokenizer_path, "test", f"{tokenizer_name}.arrow"),
+        remove_columns=["sequence", "description", "start_pos", "end_pos"],
     )
 
-    print(dataset_train)
-
-    exit(0)
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer,
         mlm=True,
         mlm_probability=0.15
     )
 
-
-    ###############
-    # F) Training Arguments
-    ###############
     training_args = TrainingArguments(
         output_dir="./overlap_pretrain_output",
         overwrite_output_dir=True,
@@ -99,23 +92,17 @@ if __name__ == "__main__":
         per_device_train_batch_size=8,
         save_steps=1000,
         logging_steps=200,
-        evaluation_strategy="no",  # or "steps"/"epoch" if you have a validation set
+        eval_strategy="steps",
         dataloader_num_workers=4,
     )
 
-    ###############
-    # G) Trainer
-    ###############
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=train_dataset,
-        # eval_dataset=val_dataset,  # if you split
+        train_dataset=dataset_train,
+        eval_dataset=dataset_validation,
         data_collator=data_collator,
     )
 
-    ###############
-    # H) Train
-    ###############
     trainer.train()
     print("Training complete!")
