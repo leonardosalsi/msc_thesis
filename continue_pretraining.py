108cfb63732d3a1afd4e66b431b6be7e4d0c5413
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-01-17 00:10:26 +0100

    Tokenize 1k-data for later use

--- continue_pretraining.py
+++ continue_pretraining.py
@@ -55,15 +55,16 @@ if __name__ == "__main__":
     multi_species_genomes_validation = load_from_disk(validation_path)
 
     logger.log(LOGLEVEL, "Dataset loaded")
-    with torch.device(device):
-        model = AutoModelForMaskedLM.from_pretrained(
-            "InstaDeepAI/nucleotide-transformer-v2-50m-multi-species",
-            cache_dir=models_cache_dir,
-            trust_remote_code=True,
-            local_files_only=True,
-            low_cpu_mem_usage=True
-        )
-        print(model.device)
+
+    model = AutoModelForMaskedLM.from_pretrained(
+        "InstaDeepAI/nucleotide-transformer-v2-50m-multi-species",
+        cache_dir=models_cache_dir,
+        trust_remote_code=True,
+        local_files_only=True,
+        low_cpu_mem_usage=True
+    )
+    model = model.to(device)
+
     logger.info("Model loaded")
     tokenizer = OverlappingEsmTokenizer(
         vocab_file=os.path.join(models_cache_dir, "nt50-vocab", "vocab.txt"),
