072568d25133b95308a537b799e3d03fd2ded706
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-01-23 17:21:04 +0100

    configure trainer so 1M token per step is given via gradient accumulation according to paper

--- jobs/train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-1_2kbp-from_scratch.sh
+++ jobs/train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-1_2kbp-from_scratch.sh
@@ -2,3 +2,4 @@
 cd ..
 
 sbatch jobs/train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-1_2kbp-from_scratch/train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-1_2kbp-from_scratch.sh
+
