493db582699a7c590a85ea54cdfae4467061cf76
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-01-24 15:46:41 +0100

    distribute training over 4 nodes

--- jobs/train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-2_2kbp/train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-2_2kbp.sh
+++ jobs/train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-2_2kbp/train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-2_2kbp.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-2_2kbp
 #SBATCH --output=out/train-model-multi_genome_dataset-overlappingesmtokenizerwithnskipping-2_2kbp.txt
 #SBATCH --cpus-per-task=2
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mem-per-cpu=40G
 #SBATCH -p gpu
 #SBATCH --gres=gpu:v100:4
