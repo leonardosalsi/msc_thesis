eecffc1160afc49e61f119d0990c3d6afae51c52
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-01-25 10:26:04 +0100

    configure trainer so 1M token per step is given via gradient accumulation according to paper

--- train_model.py
+++ train_model.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
         eval_strategy="steps",
         load_best_model_at_end=True,
         metric_for_best_model="loss",
-        dataloader_num_workers=8,
+        dataloader_num_workers=4,
         gradient_checkpointing=False,
         logging_dir='/dev/null',
         remove_unused_columns=False,
