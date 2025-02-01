95994a1f751c8d0ac8d5a0babc63daabc4446e3a
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-02-01 12:48:23 +0100

    configure trainer so 1M token per step is given via gradient accumulation according to paper

--- jobs/filter.sh
+++ jobs/filter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ..
 
-sbatch jobs/filer_downsize-chunks-multi_genome_dataset-train-1_2kbp/filter_downsize-chunks-multi_genome_dataset-train-1_2kbp.sh
-sbatch jobs/filer_downsize-chunks-multi_genome_dataset-train-2_2kbp/filter_downsize-chunks-multi_genome_dataset-train-2_2kbp.sh
+sbatch jobs/filter_downsize-chunks-multi_genome_dataset-train-1_2kbp/filter_downsize-chunks-multi_genome_dataset-train-1_2kbp.sh
+sbatch jobs/filter_downsize-chunks-multi_genome_dataset-train-2_2kbp/filter_downsize-chunks-multi_genome_dataset-train-2_2kbp.sh
