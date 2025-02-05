45f4fab92eeaccf474814dfd683f768bdfc08128
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-02-05 07:41:03 +0100

    chore: routine push

--- jobs/train.sh
+++ jobs/train.sh
@@ -14,5 +14,5 @@ conda activate gpu_env
 export HF_DATASETS_OFFLINE=1
 export HF_HUB_OFFLINE=1
 
-torchrun --nproc_per_node=4 --master_addr=127.0.0.1 --master_port=29500 /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/train_model.py \
+torchrun --nproc_per_node=5 --master_addr=127.0.0.1 --master_port=29500 /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/train_model.py \
     multi_genome_dataset OverlappingEsmTokenizerWithNSkipping 1200 --shannon 1.35 1.8 --gc 0.4 0.6
