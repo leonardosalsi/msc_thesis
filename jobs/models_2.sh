6eeb19770667d22f323856921a467510d254c5d3
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-02-09 10:01:46 +0100

    set up default training

--- jobs/models_2.sh
+++ jobs/models_2.sh
@@ -3,7 +3,6 @@ cd ..
 
 sbatch jobs/eval-models-from-trained-1-123456789101112131415161718192021222324252627-10-lora/eval-model-nucleotide-transformer-v2-50m-multi-species-H3K4me1.sh
 sbatch jobs/eval-models-from-trained-1-123456789101112131415161718192021222324252627-10-lora/eval-model-nucleotide-transformer-v2-50m-multi-species-H3K4me2.sh
-sbatch jobs/eval-models-from-trained-1-123456789101112131415161718192021222324252627-10-lora/eval-model-nucleotide-transformer-v2-50m-multi-species-H3K4me3.sh
 sbatch jobs/eval-models-from-trained-1-123456789101112131415161718192021222324252627-10-lora/eval-model-nucleotide-transformer-v2-50m-multi-species-H3K9ac.sh
 sbatch jobs/eval-models-from-trained-1-123456789101112131415161718192021222324252627-10-lora/eval-model-nucleotide-transformer-v2-50m-multi-species-H3K9me3.sh
 sbatch jobs/eval-models-from-trained-1-123456789101112131415161718192021222324252627-10-lora/eval-model-nucleotide-transformer-v2-50m-multi-species-H4K20me1.sh
