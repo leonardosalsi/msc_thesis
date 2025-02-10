7dd09d4305a11d7c57248f866836e6d982104943
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-02-10 20:08:36 +0100

    added all evaluation jobs

--- jobs/evaluate_trained_modelId_0.sh
+++ jobs/evaluate_trained_modelId_0.sh
@@ -2,7 +2,6 @@
 cd ..
 
 sbatch jobs/evaluate_trained_modelId_0/splice_sites_all.sh
-sbatch jobs/evaluate_trained_modelId_0/H3K4me1.sh
 sbatch jobs/evaluate_trained_modelId_0/H3K4me2.sh
 sbatch jobs/evaluate_trained_modelId_0/H3K4me3.sh
 sbatch jobs/evaluate_trained_modelId_0/H3K9ac.sh
@@ -10,6 +9,4 @@ sbatch jobs/evaluate_trained_modelId_0/H3K9me3.sh
 sbatch jobs/evaluate_trained_modelId_0/H4K20me1.sh
 sbatch jobs/evaluate_trained_modelId_0/gb_human_ensembl_regulatory.sh
 sbatch jobs/evaluate_trained_modelId_0/gb_demo_human_or_worm.sh
-sbatch jobs/evaluate_trained_modelId_0/gb_human_ocr_ensembl.sh
-sbatch jobs/evaluate_trained_modelId_0/gb_human_enhancers_ensembl.sh
 
--- jobs/evaluate_trained_modelId_3.sh
+++ jobs/evaluate_trained_modelId_3.sh
@@ -2,4 +2,3 @@
 cd ..
 
 sbatch jobs/evaluate_trained_modelId_3/H2AFZ.sh
-
