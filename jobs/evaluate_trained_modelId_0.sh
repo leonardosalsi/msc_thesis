a32059e27595b6e0b3098aadac729247be8b3387
Leonardo Salsi <lsa@autexis.ch>
2025-02-11 11:12:46 +0100

    chore: routine push

--- jobs/evaluate_trained_modelId_0.sh
+++ jobs/evaluate_trained_modelId_0.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 cd ..
 
-sbatch jobs/evaluate_trained_modelId_0/splice_sites_all.sh
-sbatch jobs/evaluate_trained_modelId_0/H3K4me2.sh
 sbatch jobs/evaluate_trained_modelId_0/H3K4me3.sh
-sbatch jobs/evaluate_trained_modelId_0/H3K9ac.sh
-sbatch jobs/evaluate_trained_modelId_0/H3K9me3.sh
-sbatch jobs/evaluate_trained_modelId_0/H4K20me1.sh
-sbatch jobs/evaluate_trained_modelId_0/gb_human_ensembl_regulatory.sh
-sbatch jobs/evaluate_trained_modelId_0/gb_demo_human_or_worm.sh
-
