5b156e434661c09c4ac1818b76c428cf93d65865
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-02-16 11:05:37 +0100

    added all evaluation jobs

---
+++ jobs/evaluate_trained_modelId_11/H3K27ac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=11_H3K27ac
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/11_H3K27ac.txt
+#SBATCH --cpus-per-task=2
+#SBATCH --time=100:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+
+source ~/.bashrc
+conda activate gpu_env
+
+HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/evaluate_model_trained.py 11 10 --samples 10
