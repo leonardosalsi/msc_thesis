ce5c9f62024e98567751a04f54b469974b93b461
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-02-17 19:20:12 +0100

    added all evaluation jobs

--- jobs/logan/examine/examine_logan_25_reverse.sh
+++ jobs/logan/examine/examine_logan_25_reverse.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=eval_logan_25_rev
 #SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/eval_logan_25_rev.txt
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=5
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=80G
 
