10a72538082f8ae12bc8701617b840f5c403a58b
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-02-17 12:57:04 +0100

    added all evaluation jobs

---
+++ jobs/logan_examine_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ..
+
+sbatch jobs/logan/examine/examine_logan_31.sh
+sbatch jobs/logan/examine/examine_logan_28.sh
+sbatch jobs/logan/examine/examine_logan_25.sh
+sbatch jobs/logan/examine/examine_logan_20.sh
+sbatch jobs/logan/examine/examine_logan_31_reverse.sh
+sbatch jobs/logan/examine/examine_logan_31_reverse.sh
+sbatch jobs/logan/examine/examine_logan_31_reverse.sh
+sbatch jobs/logan/examine/examine_logan_31_reverse.sh
