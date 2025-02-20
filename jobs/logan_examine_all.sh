d258705c135e459f2390ab76ae56f1447f6b451f
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-02-20 16:37:31 +0100

    change tokenization

--- jobs/logan_examine_all.sh
+++ jobs/logan_examine_all.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd ..
 
-sbatch jobs/logan/examine/examine_logan_31.sh
-sbatch jobs/logan/examine/examine_logan_28.sh
-sbatch jobs/logan/examine/examine_logan_25.sh
-sbatch jobs/logan/examine/examine_logan_20.sh
 sbatch jobs/logan/examine/examine_logan_31_reverse.sh
 sbatch jobs/logan/examine/examine_logan_28_reverse.sh
 sbatch jobs/logan/examine/examine_logan_25_reverse.sh
