7daffc63bb77e9be99fa124aa85b5a3fc0c55193
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-03-02 14:01:43 +0100

    added all evaluation jobs

--- jobs/logan/min_generate/generate_logan_28.sh
+++ jobs/logan/min_generate/generate_logan_28.sh
@@ -10,4 +10,4 @@ source ~/.bashrc
 conda activate gpu_env
 
 HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
-python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/create_logan_dataset.py 28
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/create_minimal_subset.py 28
