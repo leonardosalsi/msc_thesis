d72a330cd87914e77eb562f6cdefbbbf2b97b16c
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-03-21 02:45:19 +0100

    multi-threaded group processing

--- create_logan_dataset.py
+++ create_logan_dataset.py
@@ -1,61 +1,41 @@
 import argparse
-import concurrent.futures
-import math
+import csv
 import os
-import random
-import glob
-from io import StringIO
 from pprint import pprint
 
-import numpy as np
-import zstandard
-from datasets import Dataset, DatasetDict
-from Bio import SeqIO
-from tqdm import tqdm
-from collections import defaultdict
-from config import results_dir, logan_datasets_dir, generated_datasets_dir, generator_cache_dir
-from Bio.Seq import Seq
-import time
-import logging  # For logging retry messages
-import logan_compiler
-from util import init_logger, LOGLEVEL
-
-MAX_WORKERS = 8
-
-def generate_dataset(kmer, reverse_complement, chunk_size):
-    logan_data = os.path.join(logan_datasets_dir, 'data')
-
-    fasta_files = glob.glob(os.path.join(logan_data, "*.contigs.fa.zst"))
-    print(len(fasta_files))
-    with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        futures = {executor.submit(logan_compiler.process_fasta_file, file, kmer, reverse_complement, chunk_size): file
-                   for file in fasta_files}
-
-        for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures),
-                           desc="Processing fasta files"):
-            file_name = futures[future]
-            acc = file_name.split('/')[-1].split('.')[0]
-            try:
-                for entry in future.result():
-                    yield entry
-            except Exception as e:
-                tqdm.write(f"Error processing {acc}: {e}")
-
-def save_dataset_with_retry(dataset, save_dir, num_proc, max_retries=3, delay=5):
-    for attempt in range(1, max_retries + 1):
-        try:
-            dataset.save_to_disk(save_dir, num_proc=num_proc)
-            logging.info(f"Dataset saved successfully to {save_dir} on attempt {attempt}.")
-            return
-        except OSError as e:
-            logging.warning(f"Attempt {attempt} failed to save dataset to {save_dir}. Error: {e}. Retrying in {delay} seconds.")
-            time.sleep(delay)
-    raise OSError(f"Failed to save dataset to {save_dir} after {max_retries} attempts.")
+from datasets import Dataset, DatasetDict, Features, Value
+from config import logan_datasets_dir, generated_datasets_dir, generator_cache_dir
+import fasta_walker
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Script to train model either from scratch or from pretrained weights with specified tokenization."
     )
+
+    parser.add_argument(
+        "fasta_files_path",
+        type=str,
+        help="Folder of FASTA files to be processed",
+    )
+
+    parser.add_argument(
+        "--metadata_file_path",
+        type=str,
+        help="Path to metadata file",
+    )
+
+    parser.add_argument(
+        "--acc_column",
+        type=str,
+        help="Column header name of accession in metadata file"
+    )
+
+    parser.add_argument(
+        "--group_id_column",
+        type=str,
+        help="Column header name of group id of accession in metadata file"
+    )
+
     parser.add_argument(
         "--kmer",
         type=int,
@@ -74,19 +54,29 @@ def parse_args():
     parser.add_argument(
         "--chunk_size",
         type=int,
-        default=1200,
+        default=0,
         help="Chunk size (defined when further splitting data)",
-        choices=[1200, 2200]
     )
+
+    parser.add_argument(
+        "--max_workers",
+        type=int,
+        default=1,
+        help="Chunk size (defined when further splitting data)",
+    )
+
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
+    max_workers = args.max_workers
     kmer = args.kmer
     chunk_size = args.chunk_size
     reverse_complement = args.reverse_complement
-
-    num = math.floor(chunk_size / 1000)
+    fasta_files_path = args.fasta_files_path
+    metadata_path = args.metadata_file_path
+    metadata_acc_column = args.acc_column
+    metadata_group_id_column = args.group_id_column
 
     dataset_dir = os.path.join(generated_datasets_dir, f'logan')
     cache_dir = os.path.join(generator_cache_dir, 'logan')
@@ -98,12 +88,44 @@ if __name__ == "__main__":
     else:
         dataset_dir = os.path.join(dataset_dir, f'kmer_{kmer}')
         generator_cache = os.path.join(cache_dir, f'kmer_{kmer}')
-    dataset_dir = dataset_dir + f"_{num}k"
-    cache_dir = cache_dir  + f"_{num}k"
+    dataset_dir = dataset_dir + f"_{chunk_size}k"
+    cache_dir = cache_dir  + f"_{chunk_size}k"
     os.makedirs(dataset_dir, exist_ok=True)
     os.makedirs(cache_dir, exist_ok=True)
 
-    new_dataset = Dataset.from_generator(lambda: generate_dataset(kmer, reverse_complement, chunk_size), cache_dir=cache_dir)
+    features = Features({
+        "sequence": Value("string"),
+        f"{metadata_group_id_column}": Value("string"),
+    })
+
+    """
+    gen = fasta_walker.create_random_walk_sequences(
+        kmer,
+        chunk_size,
+        reverse_complement,
+        fasta_files_path,
+        metadata_path,
+        metadata_acc_column,
+        metadata_group_id_column
+    )
+
+    for g in gen:
+        print(g)
+    """
+
+    new_dataset = Dataset.from_generator(
+        lambda: fasta_walker.create_random_walk_sequences(
+            kmer,
+            chunk_size,
+            reverse_complement,
+            fasta_files_path,
+            metadata_path,
+            metadata_acc_column,
+            metadata_group_id_column
+        ),
+        cache_dir=cache_dir,
+        features=features,
+    )
 
     split_dataset = new_dataset.train_test_split(test_size=0.2, seed=112)
     train_dataset = split_dataset['train']
@@ -113,8 +135,9 @@ if __name__ == "__main__":
         "test": test_dataset
     })
 
-    save_dataset_with_retry(dataset, dataset_dir, num_proc=MAX_WORKERS)
 
+
+    dataset.save_to_disk(dataset_dir, num_proc=max_workers)
     dataset_dir = dataset_dir + f"_filtered"
     cache_dir = cache_dir + f"_filtered"
 
@@ -131,4 +154,4 @@ if __name__ == "__main__":
         "test": filtered_test
     })
 
-    save_dataset_with_retry(filtered_dataset, dataset_dir, num_proc=MAX_WORKERS)
+    filtered_dataset.save_to_disk(dataset_dir, num_proc=max_workers)
