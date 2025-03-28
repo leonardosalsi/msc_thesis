a473e9f3a5825c40b47a412e09172284ba5cab4a
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-03-28 19:37:43 +0100

    multi-threaded group processing

--- jobs/logan/generate/1k_scratch.sh
+++ jobs/logan/generate/1k_scratch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=gen_logan_1k
-#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/gen_logan_1k.txt
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/scratch_gen_logan_1k.txt
 #SBATCH --cpus-per-task=8
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=12G
