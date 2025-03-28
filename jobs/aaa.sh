321058541fe7cf2c3d1513865c3010bc2e6b302b
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-03-28 19:33:28 +0100

    multi-threaded group processing

---
+++ jobs/aaa.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ..
+
+sbatch jobs/logan/generate/1k_scratch.sh
---
+++ jobs/logan/generate/1k_scratch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=gen_logan_1k
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/gen_logan_1k.txt
+#SBATCH --cpus-per-task=8
+#SBATCH --time=100:00:00
+#SBATCH --mem-per-cpu=128G
+
+source ~/.bashrc
+conda activate gpu_env
+
+export SCRATCH_FASTA_DIR="$TMPDIR/logan_data"
+mkdir -p "$SCRATCH_FASTA_DIR"
+
+cp /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/logan/data/*.fa.zst "$SCRATCH_FASTA_DIR/"
+ls "$SCRATCH_FASTA_DIR"
+
+HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/create_logan_dataset.py \
+"$SCRATCH_FASTA_DIR" \
+--metadata_file_path /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/logan/data/metadata.csv \
+--chunk_size 1200 \
+--reverse_complement --max_workers 8 --acc_column acc --group_id_column kmeans
