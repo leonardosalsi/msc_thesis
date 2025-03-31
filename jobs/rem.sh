b7a0055affbc85437b1d95fb5a889e6f07ebdfb6
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-03-31 10:24:35 +0200

    multi-threaded group processing

--- jobs/rem.sh
+++ jobs/rem.sh
@@ -4,8 +4,3 @@ cd ..
 
 sbatch jobs/evaluate_evo2_1b/enhancers.sh
 sbatch jobs/evaluate_evo2_1b/enhancers_types.sh
-sbatch jobs/evaluate_evo2_1b/gb_human_ensembl_regulatory.sh
-sbatch jobs/evaluate_evo2_1b/gb_human_ocr_ensembl.sh
-sbatch jobs/evaluate_evo2_1b/gb_drosophila_enhancers_stark.sh
-sbatch jobs/evaluate_evo2_1b/gb_dummy_mouse_enhancers_ensembl.sh
-sbatch jobs/evaluate_evo2_1b/gb_human_enhancers_ensembl.sh
