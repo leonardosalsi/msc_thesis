98fc715fa1d9beba3b778e6e0e3185f5526680e9
Leonardo Salsi <lsa@autexis.ch>
2025-04-02 07:03:54 +0200

    chore: deploy redis and influx to prod

--- jobs/train/pca2.sh
+++ jobs/train/pca2.sh
@@ -18,4 +18,4 @@ export TF_CPP_MIN_LOG_LEVEL=2
 python /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py    \
 /cluster/scratch/salsil/multi_genome_species/1k  overlapping \
 --compile_model --pca_embeddings  --train_size 10 --eval_size 32 \
---gradient_accumulation 50 --max_workers 4 --use_scratch --keep_in_memory --logging_steps 1
+--gradient_accumulation 50 --max_workers 4 --use_scratch --keep_in_memory
