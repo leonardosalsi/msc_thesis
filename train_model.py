954c2d55b112dec18ece08e0216348180011e712
Leonardo Salsi <lsa@autexis.ch>
2025-04-02 06:51:11 +0200

    chore: deploy redis and influx to prod

--- train_model.py
+++ train_model.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     tokenized_train_sequences = dataset_train.shuffle()
     tokenized_train_sequences.set_transform(tokenize_function)
     tokenized_validation_sequences = dataset_validation.shuffle()
-    tokenized_validation_sequences = tokenized_validation_sequences.select(range(5000))
+    tokenized_validation_sequences = tokenized_validation_sequences.select(range(20000))
     tokenized_validation_sequences.set_transform(tokenize_function)
 
     data_collator = DataCollatorForLanguageModeling(
