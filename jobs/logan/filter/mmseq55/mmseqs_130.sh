bdae00dae359c02decfb0b3be88805f9fd744f20
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-10 08:17:26 +0200

    chore: removed old ingresses

---
+++ jobs/logan/filter/mmseq55/mmseqs_130.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=mmseqs_130
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/mmseqs_130.txt
+#SBATCH --cpus-per-task=4
+#SBATCH --time=100:00:00
+#SBATCH --mem-per-cpu=16G
+
+source ~/.bashrc
+conda activate fasta_env
+
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/filter.py \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_raw/random_walk_130.json \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_filtered_55 \
+--split_memory_limit 500 --min_seq_id 0.55
