2ea7b99a13e4d7d071e8b9caa63d016e5bc75d28
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-14 12:26:45 +0200

    multi-threaded group processing

--- jobs/logan/filter/mmseq95/mmseqs_1.sh
+++ jobs/logan/filter/mmseq95/mmseqs_1.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name=mmseqs_1
 #SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/mmseqs_1.txt
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=4
 #SBATCH --time=100:00:00
-#SBATCH --mem-per-cpu=256G
+#SBATCH --mem-per-cpu=64G
 
 source ~/.bashrc
 conda activate fasta_env
@@ -12,4 +12,4 @@ conda activate fasta_env
 python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/filter.py \
 /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_raw/random_walk_1.json \
 /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_filtered_95 \
---split_memory_limit 1000 --min_seq_id 0.95
+--split_memory_limit 190 --min_seq_id 0.95
