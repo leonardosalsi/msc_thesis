ee7d6f64d4ef470206dc67f95b8d60bc2a666a4f
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-14 12:50:47 +0200

    multi-threaded group processing

--- pre_train/util.py
+++ pre_train/util.py
@@ -4,34 +4,33 @@ import sys
 import torch
 
 from util import init_logger, LOGLEVEL
+LOGGER = init_logger()
 
 def print_args(args, title):
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    logger = init_logger()
     if ("LOCAL_RANK" in os.environ and os.environ["LOCAL_RANK"] == "0") or ("LOCAL_RANK" not in os.environ):
-        logger.log(LOGLEVEL, "\n" + "=" * 80)
-        logger.log(LOGLEVEL, f"{title} - {timestamp}".center(80))
-        logger.log(LOGLEVEL, "=" * 80)
+        LOGGER.log(LOGLEVEL, "\n" + "=" * 80)
+        LOGGER.log(LOGLEVEL, f"{title} - {timestamp}".center(80))
+        LOGGER.log(LOGLEVEL, "=" * 80)
         for arg, value in sorted(vars(args).items()):
-            logger.log(LOGLEVEL, "{:<25}: {}".format(arg, value))
-        logger.log(LOGLEVEL, "=" * 80 + "\n")
+            LOGGER.log(LOGLEVEL, "{:<25}: {}".format(arg, value))
+        LOGGER.log(LOGLEVEL, "=" * 80 + "\n")
     return timestamp
 
 
 def get_device():
-    logger = init_logger()
     if "LOCAL_RANK" in os.environ:
         local_rank = int(os.environ["LOCAL_RANK"])
         device = torch.device("cuda", local_rank)
-        logger.log(LOGLEVEL, f"Using GPU (local rank {local_rank}): {torch.cuda.get_device_name(local_rank)}")
+        LOGGER.log(LOGLEVEL, f"Using GPU (local rank {local_rank}): {torch.cuda.get_device_name(local_rank)}")
     else:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         if device.type == "cuda":
-            logger.log(LOGLEVEL, f"Using GPU: {torch.cuda.get_device_name(0)}")
-            logger.log(LOGLEVEL, f"Number of GPUs available: {torch.cuda.device_count()}")
-            logger.log(LOGLEVEL, f"CUDA_VISIBLE_DEVICES: {os.environ.get('CUDA_VISIBLE_DEVICES')}")
+            LOGGER.log(LOGLEVEL, f"Using GPU: {torch.cuda.get_device_name(0)}")
+            LOGGER.log(LOGLEVEL, f"Number of GPUs available: {torch.cuda.device_count()}")
+            LOGGER.log(LOGLEVEL, f"CUDA_VISIBLE_DEVICES: {os.environ.get('CUDA_VISIBLE_DEVICES')}")
         else:
-            logger.log(LOGLEVEL, "GPU not available. Using CPU instead.")
+            LOGGER.log(LOGLEVEL, "GPU not available. Using CPU instead.")
 
     torch.cuda.empty_cache()
     return device
