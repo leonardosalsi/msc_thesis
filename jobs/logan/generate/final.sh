983de5aa615bc3ebcb6f60cf6d300156769345b4
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-14 23:13:38 +0200

    change tokenization

---
+++ jobs/logan/generate/final.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=final
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/final.txt
+#SBATCH --cpus-per-task=8
+#SBATCH --time=100:00:00
+#SBATCH --mem-per-cpu=32G
+
+source ~/.bashrc
+conda activate gpu_env
+
+HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/logan_final.py \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_separated/1k
