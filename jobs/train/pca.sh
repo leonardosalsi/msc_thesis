8bde37bb93c21bb634d8ef39db6af8db0849934d
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-14 10:13:32 +0200

    multi-threaded group processing

--- jobs/train/pca.sh
+++ jobs/train/pca.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/train_overlap.txt
 #SBATCH --cpus-per-task=4
 #SBATCH --time=96:00:00
-#SBATCH --mem-per-cpu=128G
+#SBATCH --mem-per-cpu=256G
 #SBATCH -p gpu
-#SBATCH --gres=gpu:rtx4090:1
+#SBATCH #SBATCH --gpus=rtx_4090:5
 
 source ~/.bashrc
 conda activate gpu_env
@@ -14,6 +14,7 @@ conda activate gpu_env
 export HF_DATASETS_OFFLINE=1
 export HF_HUB_OFFLINE=1
 
-python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/train_model.py \
-    /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/multi_genome_dataset/1k \
-    overlapping --compile_model --keep_in_memory --ewc_lambda 450
+accelerate launch --num_processes 5 /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/train_model.py \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/multi_genome_dataset/1k \
+overlapping --compile_model --compile_model  --train_size 10 --eval_size 32 \
+--gradient_accumulation 10 --max_workers 4 --keep_in_memory
