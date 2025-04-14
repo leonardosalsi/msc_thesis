c4d80d65ea3a859e908384e9ada7cd991c28b41d
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-14 10:03:25 +0200

    multi-threaded group processing

--- jobs/logan/filter/mmseq95/mmseqs_1.sh
+++ jobs/logan/filter/mmseq95/mmseqs_1.sh
@@ -1,20 +1,15 @@
 cd log#!/bin/bash
 
 #SBATCH --job-name=mmseqs_1
-#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/mmseqs_1.txt
-#SBATCH --cpus-per-task=4
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/mmseqs_1.txt
+#SBATCH --cpus-per-task=5
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=256G
 
 source ~/.bashrc
-source $HOME/gpu_env/bin/activate
+conda activate fasta_env
 
-export HF_DATASETS_OFFLINE=1
-export HF_HUB_OFFLINE=1
-export WANDB_DISABLED=true
-export TF_CPP_MIN_LOG_LEVEL=2
-
-python /cluster/home/salsil/msc_thesis_root/msc_thesis/filter.py \
-/cluster/scratch/salsil/logan_raw/random_walk_1.json \
-/cluster/scratch/salsil/logan_filtered_95 \
---split_memory_limit 800 --min_seq_id 0.95
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/filter.py \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_raw/random_walk_1.json \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_filtered_95 \
+--split_memory_limit 1000 --min_seq_id 0.95
--- jobs/logan/filter/mmseq95/mmseqs_135.sh
+++ jobs/logan/filter/mmseq95/mmseqs_135.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 #SBATCH --job-name=mmseqs_135
-#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/mmseqs_135.txt
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/mmseqs_135.txt
 #SBATCH --cpus-per-task=4
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=256G
 
 source ~/.bashrc
-source $HOME/gpu_env/bin/activate
+conda activate fasta_env
 
-export HF_DATASETS_OFFLINE=1
-export HF_HUB_OFFLINE=1
-export WANDB_DISABLED=true
-export TF_CPP_MIN_LOG_LEVEL=2
-
-python /cluster/home/salsil/msc_thesis_root/msc_thesis/filter.py \
-/cluster/scratch/salsil/logan_raw/random_walk_135.json \
-/cluster/scratch/salsil/logan_filtered_95 \
---split_memory_limit 800 --min_seq_id 0.95
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/filter.py \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_raw/random_walk_135.json \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_filtered_95 \
+--split_memory_limit 700 --min_seq_id 0.95
