4c7b03402e842c4374d450c06efe760efe72d916
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-14 23:13:38 +0200

    change tokenization

---
+++ logan_final.py
@@ -0,0 +1,62 @@
+import argparse
+import csv
+import glob
+import os
+from pprint import pprint
+import json
+from datasets import Dataset, DatasetDict, Features, Value
+from config import logan_datasets_dir, generated_datasets_dir, generator_cache_dir, logs_dir
+
+
+def json_files_generator(folder_path):
+    pattern = os.path.join(folder_path, '*.json')
+    # Iterate over each JSON file in the folder
+    for file_path in glob.glob(pattern):
+        file_basename = os.path.basename(file_path)
+        # Use regex to match and extract the number from filenames like random_walk_[num].json
+        match = re.search(r'random_walk_(\d+)\.json$', file_basename)
+        if not match:
+            print(f"Skipping file {file_path}: filename does not match expected pattern.")
+            continue
+        # Convert the captured number to an integer (or keep as string if preferred)
+        organism = int(match.group(1))
+
+        with open(file_path, 'r', encoding='utf-8') as f:
+            try:
+                strings_array = json.load(f)
+                if isinstance(strings_array, list):
+                    for string_item in strings_array:
+                        if isinstance(string_item, str):
+                            # Yield a dictionary that now includes both the sequence and the organism extracted from the filename
+                            yield {'sequence': string_item, 'organism': organism}
+                        else:
+                            print(f"Skipping non-string item in {file_path}: {string_item}")
+                else:
+                    print(f"Skipping file {file_path}: Expected a JSON array but got {type(strings_array)}")
+            except Exception as e:
+                print(f"Error reading {file_path}: {e}")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Script to train model either from scratch or from pretrained weights with specified tokenization."
+    )
+
+    parser.add_argument(
+        "json_files_dir",
+        type=str,
+        help="Folder of FASTA files to be processed",
+    )
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+        args = parse_args()
+        json_files_dir = args.json_files_dir
+
+        gen = json_files_generator(json_files_dir)
+        for g in gen:
+            print(g)
+
+        dataset = Dataset.from_generator(generator=lambda: json_files_generator(json_files_dir))
+
