b0314aaa0343645334f6c3b777fde96581d0807b
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-14 13:21:48 +0200

    multi-threaded group processing1

--- jobs/train/pca2.sh
+++ jobs/train/pca2.sh
@@ -15,7 +15,7 @@ export HF_HUB_OFFLINE=1
 export WANDB_DISABLED=true
 export TF_CPP_MIN_LOG_LEVEL=2
 
-accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend yes /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
+accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend inductor /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
 /cluster/scratch/salsil/multi_genome_species/1k  overlapping \
 --compile_model --logging_steps 1 --train_size 10 --eval_size 32 \
 --gradient_accumulation 10 --max_workers 4 --ewc_lambda 10 \
