b8a36cd407c418b5d82efe0921b462bc6de959a2
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-15 19:05:33 +0200

    change tokenization

---
+++ jobs/logan/filter/reformat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=reformat
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/reformat.txt
+#SBATCH --cpus-per-task=2
+#SBATCH --time=100:00:00
+#SBATCH --mem-per-cpu=64G
+
+source ~/.bashrc
+conda activate fasta_env
+
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/reformat.py \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_filtered_95 \
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_separated/1k
