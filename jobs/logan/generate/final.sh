62a34e949a519f0bc58eb53c91d98c44cf05a993
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-15 14:15:20 +0200

    chore: removed old ingresses

--- jobs/logan/generate/final.sh
+++ jobs/logan/generate/final.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=final2
-#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/final2.txt
+#SBATCH --job-name=final
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/final.txt
 #SBATCH --cpus-per-task=8
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=32G
-#SBATCH --tmp=500g
 
 source ~/.bashrc
 conda activate gpu_env
 
 HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
 python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/logan_final.py \
-/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_separated/1k \
---use_scratch
+/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_separated/1k
