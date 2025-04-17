0e16c4082645d705a1cdf1ad4ad8370cf54134cd
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-17 08:17:37 +0200

    chore: removed old ingresses

--- jobs/evaluate_trained_modelId_17/H3K4me1.sh
+++ jobs/evaluate_trained_modelId_17/H3K4me1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=16_H3K4me1
-#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/16_H3K4me1.txt
+#SBATCH --job-name=18_H3K4me1
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/18_H3K4me1.txt
 #SBATCH --cpus-per-task=2
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=4G
@@ -12,4 +12,4 @@ source ~/.bashrc
 conda activate gpu_env
 
 HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
-python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/evaluate_model_trained.py 17 13 --samples 3 --pca_embeddings
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/evaluate_model_trained.py 18 13 --samples 3 --pca_embeddings
