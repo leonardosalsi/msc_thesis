e4355e78e81859c5de0dfc3c29ccd09760346e2e
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-24 21:56:05 +0200

    multi-threaded group processing11

--- jobs/train/euler.sh
+++ jobs/train/euler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=train_overlap_pca_512
-#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/train_overlap_pca_512.txt
+#SBATCH --job-name=train_overlap_pca_128
+#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/train_overlap_pca_128.txt
 #SBATCH --cpus-per-task=4
 #SBATCH --time=96:00:00
 #SBATCH --mem-per-cpu=64G
@@ -18,4 +18,4 @@ export TF_CPP_MIN_LOG_LEVEL=2
 accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend inductor /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
 /cluster/scratch/salsil/multi_genome_species/1k  overlapping \
 --compile_model --logging_steps 500 --train_size 5 --eval_size 32 \
---gradient_accumulation 20 --max_workers 4 --pca_embeddings --pca_dim 512
+--gradient_accumulation 20 --max_workers 4 --pca_embeddings --pca_dim 128
