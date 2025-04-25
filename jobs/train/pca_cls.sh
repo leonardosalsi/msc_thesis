b0baeb3a18dbbf7ce3308331fb582782aa4a9978
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-25 16:31:41 +0200

    fix issue with cumulative loss when adding customization on model

--- jobs/train/pca_cls.sh
+++ jobs/train/pca_cls.sh
@@ -16,6 +16,6 @@ export WANDB_DISABLED=true
 export TF_CPP_MIN_LOG_LEVEL=2
 
 accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend inductor /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
-/cluster/scratch/salsil/multi_genome_species/1k  overlapping \
+--dataset /cluster/scratch/salsil/multi_genome_species/1k --tokenizer overlapping \
 --compile-model --logging-steps 500 --train-size 5 --eval-size 32 \
 --gradient-accumulation 20 --max-workers 4 --pca-embeddings cls --pca-dim 256
