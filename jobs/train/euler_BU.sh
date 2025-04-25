7a0c5af6fa0989b53aa40d6e44bf723a60e4b120
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-25 13:41:30 +0200

    multi-threaded group processing11

--- jobs/train/euler_BU.sh
+++ jobs/train/euler_BU.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=logan_overlap_mean_pca_256
-#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/logan_overlap_mean_pca_256.txt
+#SBATCH --job-name=512_ewc_pca
+#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/logan_ewc_pca_512.txt
 #SBATCH --cpus-per-task=4
 #SBATCH --time=96:00:00
 #SBATCH --mem-per-cpu=64G
@@ -16,6 +16,7 @@ export WANDB_DISABLED=true
 export TF_CPP_MIN_LOG_LEVEL=2
 
 accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend inductor /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
-/cluster/scratch/salsil/multi_genome_species/1k  overlapping \
---compile_model --logging_steps 500 --train_size 5 --eval_size 32 \
---gradient_accumulation 20 --max_workers 4 --pca_embeddings --pca_dim 256
+/cluster/scratch/salsil/logan_json/logan_json  overlapping \
+--compile_model --logging_steps 500 --train_size 10 --eval_size 32 \
+--gradient_accumulation 10 --max_workers 4 --ewc_lambda 2 --load_from_json \
+--original_dataset /cluster/scratch/salsil/multi_genome_species/1k --pca_embeddings --pca_dim 512
