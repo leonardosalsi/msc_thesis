42b248cd01527b6cbf391ce3c08a3031f6dad60a
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-26 12:38:59 +0200

    fix issue with cumulative loss when adding customization on model

--- jobs/logan/filter/b.sh
+++ jobs/logan/filter/b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=filter
-#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/filter.txt
+#SBATCH --job-name=ttt
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/ttt.txt
 #SBATCH --cpus-per-task=2
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=20G
@@ -10,4 +10,4 @@ source ~/.bashrc
 conda activate gpu_env
 
 HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
-python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/check_loga.py
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/ttt.py
