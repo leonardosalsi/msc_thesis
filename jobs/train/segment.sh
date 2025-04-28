f12a28746b5ca6d5c580e4aedee753b89b2f9cef
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-28 12:10:11 +0200

    fix issue with cumulative loss when adding customization on model

--- jobs/train/segment.sh
+++ jobs/train/segment.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time=96:00:00
 #SBATCH --mem-per-cpu=64G
-#SBATCH --gpus=rtx_4090:5
+#SBATCH --gpus=rtx_4090:1
 
 source ~/.bashrc
 source $HOME/gpu_env/bin/activate
@@ -15,5 +15,5 @@ export HF_HUB_OFFLINE=1
 export WANDB_DISABLED=true
 export TF_CPP_MIN_LOG_LEVEL=2
 
-accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend inductor /cluster/home/salsil/msc_thesis_root/msc_thesis/segment_nt.py \
+python  /cluster/home/salsil/msc_thesis_root/msc_thesis/segment_nt.py \
 --dataset /cluster/scratch/salsil/logan_json/logan_json --load-from-json
