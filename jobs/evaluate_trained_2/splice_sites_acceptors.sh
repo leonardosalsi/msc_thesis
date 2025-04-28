d611cf83f1c3dfff5b47c657784ce75e3f1711e0
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-04-28 14:53:13 +0200

    fix issue with cumulative loss when adding customization on model1

---
+++ jobs/evaluate_trained_2/splice_sites_acceptors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=splice_sites_acceptors
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/splice_sites_acceptors.txt
+#SBATCH --cpus-per-task=2
+#SBATCH --time=100:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+
+source ~/.bashrc
+conda activate gpu_env
+
+HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/evaluate_model_trained.py \
+--model-name overlap_logan_ewc_5 --checkpoint 12000 --task-id 7 --samples 3
