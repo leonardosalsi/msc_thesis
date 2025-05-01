35ecca3165cd08c5f53d5062fd3d6a8c950de55b
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-01 07:58:57 +0200

    chore: add prod deployment

---
+++ jobs/evaluate_trained_euler/gb_human_ocr_ensembl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/%x.txt
+#SBATCH --cpus-per-task=2
+#SBATCH --time=100:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gpus=1
+
+source ~/.bashrc
+conda activate gpu_env
+which python
+
+HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
+python /cluster/home/salsil/msc_thesis_root/msc_thesis/evaluate_model_trained.py \
+--model-name $MODEL --checkpoint 12000 --task-id 21 $ADDITIONAL
