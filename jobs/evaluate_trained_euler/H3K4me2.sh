cb403ef78c4e5886bc279d172b368e28ecff1d8f
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-01 12:44:09 +0200

    change tokenization

--- jobs/evaluate_trained_euler/H3K4me2.sh
+++ jobs/evaluate_trained_euler/H3K4me2.sh
@@ -9,5 +9,6 @@
 source ~/.bashrc
 source $HOME/gpu_env/bin/activate
 
-HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \luster/home/salsil/msc_thesis_root/msc_thesis/evaluate_model_trained.py \
+HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
+python /cluster/home/salsil/msc_thesis_root/msc_thesis/evaluate_model_trained.py \
 --model-name $MODEL --checkpoint 12000 --task-id 14 $ADDITIONAL
