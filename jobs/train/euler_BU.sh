e18a9eadae3a909db6e3f25bddeb2bb913ef61a7
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-04 14:29:59 +0200

    fix issue with cumulative loss when adding customization on model1

--- jobs/train/euler_BU.sh
+++ jobs/train/euler_BU.sh
@@ -16,6 +16,7 @@ export WANDB_DISABLED=true
 export TF_CPP_MIN_LOG_LEVEL=2
 
 accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend inductor /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
---dataset /cluster/scratch/salsil/logan_json/logan_json --tokenizer overlapping \
+--dataset /cluster/scratch/salsil/logan_json/logan_json --tokenizer default \
 --logging-steps 500 --train-size 10 --eval-size 32 \
---gradient-accumulation 10 --max-workers 4 --load-from-json
+--gradient-accumulation 10 --max-workers 4 --load-from-json --ewc-lambda 2 \
+--original-dataset /cluster/scratch/salsil/multi_genome_species/1k
