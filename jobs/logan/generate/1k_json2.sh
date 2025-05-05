55bfbd9869c29e9302b3eeea088eaad864829bdc
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-05 13:25:58 +0200

    fix issue with cumulative loss when adding customization on model1

--- jobs/logan/generate/1k_json2.sh
+++ jobs/logan/generate/1k_json2.sh
@@ -16,7 +16,7 @@ HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
 python /cluster/home/salsil/msc_thesis_root/msc_thesis/create_logan_dataset.py \
 "$TMPDIR" \
 --metadata_file_path /cluster/scratch/salsil/logan_data/metadata.csv \
---chunk_size 1200 \
+--chunk_size 6200 \
 --reverse_complement --max_workers 8 --acc_column acc --group_id_column kmeans --use_scratch --use_json
 
 export JSONDIR="$(date +'%Y%m%d_%H%M%S')"
