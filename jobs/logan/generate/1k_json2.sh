b6c752c94da953eba0256994dc556268b31a94a8
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-05 20:57:07 +0200

    fix issue with cumulative loss when adding customization on model1

--- jobs/logan/generate/1k_json2.sh
+++ jobs/logan/generate/1k_json2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=gen_logan_1k
-#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/scratch_gen_logan_1k.txt
-#SBATCH --cpus-per-task=8
+#SBATCH --job-name=gen_logan_2k
+#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/scratch_gen_logan_2k.txt
+#SBATCH --cpus-per-task=4
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=128G
 #SBATCH --tmp=800g
@@ -10,14 +10,14 @@
 source ~/.bashrc
 source $HOME/gpu_env/bin/activate
 
-cp /cluster/scratch/salsil/logan_data/*.fa.zst "$TMPDIR/"
+cp /cluster/scratch/salsil/logan/*.fa.zst "$TMPDIR/"
 
 HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
 python /cluster/home/salsil/msc_thesis_root/msc_thesis/create_logan_dataset.py \
 "$TMPDIR" \
---metadata_file_path /cluster/scratch/salsil/logan_data/metadata.csv \
---chunk_size 6200 \
---reverse_complement --max_workers 8 --acc_column acc --group_id_column kmeans --use_scratch --use_json
+--metadata_file_path /cluster/scratch/salsil/logan/metadata.csv \
+--chunk_size 2200 \
+--reverse_complement --max_workers 4 --acc_column acc --group_id_column kmeans --use_scratch --use_json
 
 export JSONDIR="$(date +'%Y%m%d_%H%M%S')"
 mkdir "/cluster/scratch/salsil/$JSONDIR"
