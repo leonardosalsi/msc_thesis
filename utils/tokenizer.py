d067d4d842260a7f5ce31de3539970c6ae48f9e4
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-06 11:27:25 +0200

    chore: add prod deployment

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -100,18 +100,10 @@ def get_eval_tokenizer(args, repo=None):
         else:
             num_tokens = 1000
 
-        """tokenizer = OverlappingTokenizer(
+        tokenizer = OverlappingTokenizer(
             vocab_file="model_configs/vocab.txt",
             model_max_length=2048,
             num_tokens=num_tokens
-        )"""
-        tokenizer = AutoTokenizer.from_pretrained(
-            "InstaDeepAI/nucleotide-transformer-v2-50m-multi-species",
-            model_max_length=2048,
-            cache_dir=models_cache_dir,
-            remove_columns=['sequence'],
-            trust_remote_code=True,
-            local_files_only=True
         )
 
     if tokenizer is None:
