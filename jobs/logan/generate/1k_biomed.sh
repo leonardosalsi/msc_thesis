3d925b5281c7fed6e29f907e16bef95a7afe8888
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-07 18:19:40 +0200

    feat: add bloom for preliminaries, add identity for futher filtering

--- jobs/logan/generate/1k_biomed.sh
+++ jobs/logan/generate/1k_biomed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=gen_logan_6k
-#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/scratch_gen_logan_6k.txt
+#SBATCH --job-name=gen_logan_1k
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/scratch_gen_logan_1k.txt
 #SBATCH --cpus-per-task=4
 #SBATCH --time=100:00:00
 #SBATCH --mem-per-cpu=128G
@@ -12,8 +12,8 @@ conda activate gpu_env
 HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
 python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/create_logan_dataset.py \
 /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/logan/data \
---output_path /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_6200 \
+--output_path /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/logan_1200 \
 --metadata_file_path /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/logan/data/metadata.csv \
---chunk_size 6200 \
+--chunk_size 1200 \
 --reverse_complement --max_workers 4 --acc_column acc --group_id_column kmeans --use_json --identity_threshold 0.85
 
