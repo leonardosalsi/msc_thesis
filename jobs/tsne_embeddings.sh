fd22fd22dd913aa0bd9b20f5f408f418361eabb8
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-12 11:19:18 +0200

    chore: clean up repository

--- jobs/tsne_embeddings.sh
+++ jobs/tsne_embeddings.sh
@@ -7,3 +7,4 @@ if [ -z "$MODEL" ]; then
 fi
 
 sbatch --export=MODEL --job-name="extract_emb_${MODEL}" jobs/tSNE_embeddings/get_embeddings.sh
+sbatch --export=MODEL --job-name="extract_emb_${MODEL}" jobs/tSNE_embeddings/get_embeddings_var.sh
