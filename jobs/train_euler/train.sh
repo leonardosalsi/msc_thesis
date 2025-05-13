3f15d527c6c567f5622cd16d155004f1d98af1bf
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-13 12:34:26 +0200

    chore: release 2025.4.0

--- jobs/train_euler/train.sh
+++ jobs/train_euler/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=logan_ewc_0_5
-#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/logan_ewc_0_5.txt
+#SBATCH --job-name=logan_ewc_1
+#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/logan_ewc_1.txt
 #SBATCH --cpus-per-task=4
 #SBATCH --time=96:00:00
 #SBATCH --mem-per-cpu=64G
@@ -16,5 +16,5 @@ export TF_CPP_MIN_LOG_LEVEL=2
 accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend inductor /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
 --dataset /cluster/scratch/salsil/datasets/logan_6200 --tokenizer default \
 --logging-steps 500 --train-size 10 --eval-size 32 \
---gradient-accumulation 10 --max-workers 4 --load-from-json --ewc-lambda 0.5 \
+--gradient-accumulation 10 --max-workers 4 --load-from-json --ewc-lambda 1 \
 --original-dataset InstaDeepAI/multi_species_genomes
