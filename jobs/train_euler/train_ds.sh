64e3f1656bb97fe21fa3a60e570d62e85f58c6ed
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-15 09:28:11 +0200

    chore: release 2025.4.01

--- jobs/train_euler/train_ds.sh
+++ jobs/train_euler/train_ds.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time=96:00:00
 #SBATCH --mem-per-cpu=16G
-#SBATCH --gpus=rtx_4090:5
+#SBATCH --gpus=a100_80gb:3
 
 module load stack/2024-06 gcc/12.2.0 cuda/12.1.1
 
@@ -20,6 +20,6 @@ export TF_CPP_MIN_LOG_LEVEL=2
 
 deepspeed /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
 --dataset /cluster/scratch/salsil/datasets/logan_6200 --tokenizer default \
---logging-steps 500 --train-size 10 --eval-size 32 \
---gradient-accumulation 5 --max-workers 4 --load-from-json --ewc-lambda 2 \
+--logging-steps 500 --train-size 50 --eval-size 32 \
+--gradient-accumulation 1 --max-workers 4 --load-from-json --ewc-lambda 2 \
 --original-dataset InstaDeepAI/multi_species_genomes --gradient-checkpointing
