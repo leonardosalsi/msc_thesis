708d4a7169a4766151a063ef709c112de6c5c32e
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-16 01:08:53 +0200

    streamlined embedding extraction

--- fit_embeddings.py
+++ fit_embeddings.py
@@ -1,5 +1,8 @@
+from pprint import pprint
+
+from matplotlib import gridspec, patches
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, LogisticRegression
 from sklearn.metrics import average_precision_score, precision_recall_curve
 from sklearn.preprocessing import StandardScaler
 import pickle
@@ -8,6 +11,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
+
+COLORMAP = "viridis"
 
 def load_pkl(pkl_path):
     with open(pkl_path, "rb") as f:
@@ -40,37 +45,23 @@ def visualize_embedding_predictions(
     precision, recall, _ = precision_recall_curve(y_true, y_pred)
     ap = average_precision_score(y_true, y_pred)
 
-    fig, axes = plt.subplots(1, 3, figsize=figsize)
+    fig = plt.figure(figsize=figsize)
+    gs = gridspec.GridSpec(1, 4, width_ratios=[1, 1, 0.3, 1], wspace=0.05)
 
-    # --- Prediction ---
-    ax1 = axes[0]
-    scatter1 = ax1.scatter(X_2d[:, 0], X_2d[:, 1], c=y_pred, cmap="viridis", s=10)
+    ax0 = fig.add_subplot(gs[0])
+    ax1 = fig.add_subplot(gs[1])
+    spacer = fig.add_subplot(gs[2])
+    ax2 = fig.add_subplot(gs[3])
 
-    # Create an inset axis for the colorbar
-    cax = inset_axes(
-        ax1,
-        width="3%",  # width of colorbar
-        height="100%",  # height of colorbar
-        loc='lower left',
-        bbox_to_anchor=(-0.12, 0.15, 1, 1),  # move it left with negative x
-        bbox_transform=ax1.transAxes,
-        borderpad=0,
-    )
-
-    plt.colorbar(scatter1, cax=cax, label=None)
-
-    ax1.set_title("t-SNE Colored by Predicted Score")
-    ax1.set_xlabel("Component 1")
-    ax1.set_ylabel("Component 2")
-
+    spacer.axis("off")
     # --- Ground truth ---
-    ax0 = axes[1]
-    scatter0 = ax0.scatter(X_2d[:, 0], X_2d[:, 1], c=y_true, cmap="viridis", s=10)
 
+    ax0.scatter(X_2d[:, 0], X_2d[:, 1], c=y_true, cmap=COLORMAP, s=10)
     from matplotlib.lines import Line2D
+    cmap = plt.cm.get_cmap(COLORMAP, 2)
     legend_elements = [
-        Line2D([0], [0], marker='o', color='w', label='Common', markerfacecolor='blue', markersize=6),
-        Line2D([0], [0], marker='o', color='w', label='Rare', markerfacecolor='red', markersize=6)
+        Line2D([0], [0], marker='o', color='w', label='Common', markerfacecolor=cmap(0), markersize=6),
+        Line2D([0], [0], marker='o', color='w', label='Rare', markerfacecolor=cmap(1), markersize=6)
     ]
 
     ax0.legend(
@@ -85,31 +76,64 @@ def visualize_embedding_predictions(
     ax0.set_xlabel("Component 1")
     ax0.set_ylabel("Component 2")
 
+    # --- Prediction ---
+    scatter1 = ax1.scatter(X_2d[:, 0], X_2d[:, 1], c=y_pred, cmap=COLORMAP, s=10)
+
+    cax = inset_axes(ax1,
+                     width="5%",
+                     height="100%",
+                     loc='lower left',
+                     bbox_to_anchor=(1.01, 0.0, 1, 1),
+                     bbox_transform=ax1.transAxes,
+                     borderpad=0)
+
+    plt.colorbar(scatter1, cax=cax, label=None)
+    pos = ax1.get_position()
+    ax1.set_ylabel("")
+    ax1.set_yticks([])
+    ax1.set_title("t-SNE Colored by Predicted Score")
+    ax1.set_xlabel("Component 1")
+    ax1.set_ylabel("")
 
     # --- PR curve ---
-    ax2 = axes[2]
     ax2.plot(recall, precision, color="blue", lw=2, label=f"AUPRC = {ap:.3f}")
     ax2.set_xlabel("Recall")
     ax2.set_ylabel("Precision")
     ax2.set_title("Precision-Recall Curve")
+    ax2.text(1.01, 0.5, f"Layer 3", transform=ax2.transAxes,
+            rotation=270, va='center', ha='left', fontsize=12)
+
+    rect = patches.FancyBboxPatch(
+        (1.00, -0.002),  # x, y (in axis coordinates)
+        0.06, 1.0,  # width, height
+        transform=ax2.transAxes,
+        boxstyle="round,pad=0.00",
+        linewidth=0.5,
+        edgecolor='black',
+        facecolor='white',
+        clip_on=False
+    )
+    ax2.add_patch(rect)
     ax2.legend()
     ax2.grid(True)
 
     plt.suptitle(title, fontsize=14)
-    plt.tight_layout()
+    fig.subplots_adjust(left=0.03, right=0.97, top=0.90, bottom=0.12)
     plt.show()
 
 
 if __name__ == "__main__":
-    embeddings, meta_df = load_pkl("/shared/data/5_utr_embeddings_mean/default_logan_ewc_2/layer_11.pkl")
+    embeddings, meta_df = load_pkl("/shared/data/5_utr_embeddings_mean/default_logan_ewc_2/layer_0.pkl")
 
     y_true = meta_df["label"]
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(embeddings)
 
-    model = Ridge()
-    model.fit(X_scaled, meta_df["cos_similarity"])
-    y_pred = model.predict(X_scaled)
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_scaled, meta_df["label"])
+
+    y_prob = model.predict_proba(X_scaled)[:, 1]
+    y_pred = (y_prob >= 0.5).astype(int)
     auprc = average_precision_score(y_true, y_pred)
 
     visualize_embedding_predictions(
