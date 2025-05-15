2401357e88c9bf5ddefa96864d4245b3f10fb570
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-15 23:48:23 +0200

    streamlined embedding extraction

--- jobs/train_euler/train_ds.sh
+++ jobs/train_euler/train_ds.sh
@@ -23,5 +23,4 @@ deepspeed /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
 --logging-steps 10 --save-steps 6000 --train-size 32 --eval-size 256 --max-steps 12000 \
 --gradient-accumulation 1 --max-workers 4 --load-from-json --ewc-lambda 5 \
 --original-dataset InstaDeepAI/multi_species_genomes --gradient-checkpointing \
---deepspeed-config /cluster/home/salsil/msc_thesis_root/ds_config.json \
---mapping-cache /cluster/scratch/salsil/.cache/map
+--deepspeed-config /cluster/home/salsil/msc_thesis_root/ds_config.json
