8d449495f7e7d4df37fd9ffd8f19e365e444d242
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-15 15:55:04 +0200

    chore: release 2025.4.1-elsa

--- jobs/train_euler/train_ds.sh
+++ jobs/train_euler/train_ds.sh
@@ -20,6 +20,6 @@ export TF_CPP_MIN_LOG_LEVEL=2
 
 deepspeed /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
 --dataset /cluster/scratch/salsil/datasets/logan_6200 --tokenizer default \
---logging-steps 1 --save-steps 100000 --train-size 256 --eval-size 32 --max-steps 600000 \
+--logging-steps 1 --save-steps 100000 --train-size 64 --eval-size 256 --max-steps 600000 \
 --gradient-accumulation 2 --max-workers 4 --load-from-json --ewc-lambda 5 \
 --original-dataset InstaDeepAI/multi_species_genomes --gradient-checkpointing
