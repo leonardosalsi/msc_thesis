d90c9dfdd677902baa1e77243d1d620fbf3445ef
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-15 08:48:56 +0200

    chore: release 2025.4.0

--- utils/model.py
+++ utils/model.py
@@ -26,14 +26,16 @@ def get_model(args, device):
             f"model_configs/config-nucleotide-transformer-v2-50m-multi-species.json",
             local_files_only=True, trust_remote_code=True
         )
-        model = AutoModelForMaskedLM.from_config(config)
+        model = EsmForMaskedLM.from_config(config)
     else:
-        model = AutoModelForMaskedLM.from_pretrained(
+        model = EsmForMaskedLM.from_pretrained(
             "InstaDeepAI/nucleotide-transformer-v2-50m-multi-species",
             cache_dir=models_cache_dir,
             trust_remote_code=True,
             local_files_only=True,
         )
+
+    model.esm.encoder.gradient_checkpointing = True
 
     if freeze is not None:
         n_layers_to_freeze = int(len(model.esm.encoder.layer) * freeze)
