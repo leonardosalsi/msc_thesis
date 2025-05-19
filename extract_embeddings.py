827788045866e8247e4e43ba54cb673a6e07bf58
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-19 13:31:52 +0200

    streamlined embedding extraction

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -5,7 +5,7 @@ import numpy as np
 from argparse_dataclass import ArgumentParser
 from datasets import load_from_disk
 from config import results_dir
-from embedding_inspection.embedding_extraction import extract_region_embeddings_5_prime_UTR, extract_region_embeddings_genomic_elements
+from embedding_inspection.extraction_methods import extract_region_embeddings_5_prime_UTR, extract_region_embeddings_genomic_elements
 from utils.util import get_device, print_args
 
 @dataclass
