7b5de4e890ab4ab490808736f5c0a748d27b46a1
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-19 09:48:19 +0200

    streamlined embedding extraction

--- jobs/train_euler/train1.sh
+++ jobs/train_euler/train1.sh
@@ -17,6 +17,6 @@ export HF_DATASETS_CACHE=/cluster/scratch/salsil/hf_cache
 accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend inductor /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
 --dataset InstaDeepAI/multi_species_genomes --tokenizer default \
 --logging-steps 500 --train-size 5 --eval-size 16 \
---gradient-accumulation 20 --max-workers 4\
+--gradient-accumulation 20 --max-workers 4 \
 --model-name default_multi_species_pca_mean_256 \
 --pca-dim 256 --pca-embeddings mean
