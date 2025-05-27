1c875d4935f225799fa9d584862dca20f0db836e
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-27 10:42:59 +0200

    streamlined embedding extraction

--- combine_logan.py
+++ combine_logan.py
@@ -5,25 +5,26 @@ if __name__ == "__main__":
 
     json_dir = "/cluster/scratch/salsil/datasets/logan_1200"
     json_files = [os.path.join(json_dir, f) for f in os.listdir(json_dir) if f.endswith(".json")]
+
     dataset_full = load_dataset(
         "json",
         data_files=json_files,
-        # jsonl=True,          # uncomment if each file is JSON-lines
-        cache_dir="/cluster/scratch/salsil/.cache/upload",  # optional: speed up re-runs
+        block_size=1_000_000,  # ← add this!
+        cache_dir="/cluster/scratch/salsil/.cache/upload",
     )
-    dataset = dataset_full.shuffle(seed=101)
-    validation_size = 500000
-    dataset_train = dataset.select(range(validation_size, len(dataset)))
-    dataset_validation = dataset.select(range(validation_size))
 
-    ds =  DatasetDict({
-        "train": dataset_train,
-        "validation": dataset_validation
+    # now split
+    split = dataset_full["train"].train_test_split(
+        test_size=500_000,  # or fraction: test_size=0.1
+        seed=101
+    )
+    ds = DatasetDict({
+        "train": split["train"],
+        "validation": split["test"],
     })
 
-    dataset.push_to_hub(
+    ds.push_to_hub(
         repo_id=f"lsalsi/logan_multi_species_1k"
     )
 
-    print(dataset)
 
