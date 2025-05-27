366775f1651fe83b0e4e4f212bc6d6b05b7fa728
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-27 10:37:33 +0200

    streamlined embedding extraction

--- jobs/train_euler/train.sh
+++ jobs/train_euler/train.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=logan_no_ewc
-#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/logan_no_ewd.txt
+#SBATCH --job-name=logan_1k_upload
+#SBATCH --output=/cluster/home/salsil/msc_thesis_root/out/logan_1k_upload.txt
 #SBATCH --cpus-per-task=4
 #SBATCH --time=96:00:00
-#SBATCH --mem-per-cpu=16G
-#SBATCH --gpus=rtx_4090:5
+#SBATCH --mem-per-cpu=64G
 
 source ~/.bashrc
 source $HOME/gpu_env/bin/activate
@@ -13,8 +12,4 @@ source $HOME/gpu_env/bin/activate
 export WANDB_DISABLED=true
 export TF_CPP_MIN_LOG_LEVEL=2
 
-accelerate launch --num_processes 5 --num_machines 1 --mixed_precision no --dynamo_backend inductor /cluster/home/salsil/msc_thesis_root/msc_thesis/train_model.py \
---dataset /cluster/scratch/salsil/datasets/logan_6200 --tokenizer default \
---logging-steps 500 --train-size 10 --eval-size 32 \
---gradient-accumulation 10 --max-workers 4 --load-from-json --ewc-lambda 25 \
---original-dataset InstaDeepAI/multi_species_genomes
+python /cluster/home/salsil/msc_thesis_root/msc_thesis/combine_logan.py
