60e95fa59ab0b36a82c05c0f3aa9235ca68f5925
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-27 09:35:08 +0200

    streamlined embedding extraction

--- jobs/extract_embeddings/extract_5_prime_UTR_embeddings.sh
+++ jobs/extract_embeddings/extract_5_prime_UTR_embeddings.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/combine.txt
-#SBATCH --cpus-per-task=8
+#SBATCH --output=/cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/out/%x.txt
+#SBATCH --cpus-per-task=2
 #SBATCH --time=100:00:00
-#SBATCH --mem-per-cpu=64G
+#SBATCH --mem-per-cpu=4G
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
 
 source ~/.bashrc
 conda activate gpu_env
 which python
 
 HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 \
-python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/combine_logan.py
+python /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/msc_thesis/extract_embeddings.py \
+--model-name $MODEL --checkpoint 12000 --dataset-path /cluster/work/grlab/projects/projects2024-petagraph-input-optimisation-msc-thesis/generated_datasets/5_utr_af_prediction
