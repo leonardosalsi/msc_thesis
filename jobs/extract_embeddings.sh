fd4c4bfe105036f4102c06bf045ef58800ec5aa4
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-28 19:28:36 +0200

    added visualization code for downstream tasks

--- jobs/extract_embeddings.sh
+++ jobs/extract_embeddings.sh
@@ -6,5 +6,5 @@ if [ -z "$MODEL" ]; then
   exit 1
 fi
 
-sbatch --export=MODEL --job-name="exemb_5UTR_${MODEL}" jobs/extract_embeddings/extract_5_prime_UTR_embeddings.sh
-#sbatch --export=MODEL --job-name="exemb_genomelem_${MODEL}" jobs/extract_embeddings/extract_genomic_elements_embeddings.sh
+#sbatch --export=MODEL --job-name="exemb_5UTR_${MODEL}" jobs/extract_embeddings/extract_5_prime_UTR_embeddings.sh
+sbatch --export=MODEL --job-name="exemb_genomelem_${MODEL}" jobs/extract_embeddings/extract_genomic_elements_embeddings.sh
