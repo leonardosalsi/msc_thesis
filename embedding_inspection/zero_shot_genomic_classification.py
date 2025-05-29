f1d58cbbd7d3fa1dbd2b5e60a31fd732afbcd883
Leonardo Salsi <leonardo.salsi@outlook.com>
2025-05-29 18:21:51 +0200

    repo cleanup

--- embedding_inspection/zero_shot_genomic_classification.py
+++ embedding_inspection/zero_shot_genomic_classification.py
@@ -9,6 +9,7 @@ import pandas as pd
 from matplotlib import patches, gridspec, MatplotlibDeprecationWarning
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
 from sklearn.manifold import TSNE
 from sklearn.metrics import accuracy_score, confusion_matrix, average_precision_score, precision_recall_curve
 from sklearn.neighbors import KNeighborsClassifier
@@ -19,189 +20,6 @@ from config import results_dir, images_dir, cache_dir
 from utils.model_definitions import MODELS
 
 COLORMAP = 'viridis'
-
-def visualize_embeddings(model_name):
-    class_dir = os.path.join(images_dir, 'class_by_region')
-    figure_path = os.path.join(class_dir, f"{model_name}.png")
-
-    if os.path.exists(figure_path):
-        return
-
-    embedding_dir = os.path.join(results_dir, 'embeddings', 'genomic_regions_annotated', model_name)
-    tsne_dir = os.path.join(results_dir, 'tSNE', 'genomic_regions_annotated', model_name)
-
-    try:
-        embedding_files = sorted([os.path.join(embedding_dir, f) for f in os.listdir(embedding_dir) if f.endswith(".pkl") and f.startswith("layer") ],
-                       key=lambda f: int(f.split("layer_")[-1].split(".")[0]))
-        tsne_files = sorted([os.path.join(tsne_dir, f) for f in os.listdir(tsne_dir) if
-                                  f.endswith(".pkl") and f.startswith("layer")],
-                                 key=lambda f: int(f.split("layer_")[-1].split(".")[0]))
-    except:
-        return
-
-    assert len(embedding_files) == len(tsne_files)
-    n = len(embedding_files)
-
-    figsize = (18, 4 * len(embedding_files))
-    fig = plt.figure(figsize=figsize)
-    gs = gridspec.GridSpec(len(embedding_files), 5, width_ratios=[1, 0.1, 1, 0.1, 1], wspace=0.05, hspace=0.05)
-
-
-    for i, (embedding_file, tsne_file) in enumerate(zip(embedding_files, tsne_files)):
-        layer = int(embedding_file.split("layer_")[-1].split(".")[0])
-
-        with open(embedding_file, "rb") as f:
-            data = pickle.load(f)
-
-        meta = data["meta"]
-        df = pd.DataFrame(meta)
-
-        with open(tsne_file, "rb") as f:
-            X_2d = pickle.load(f)
-
-        ax_len = fig.add_subplot(gs[i, 0])
-        spacer_1 = fig.add_subplot(gs[i, 1])
-        ax_gc = fig.add_subplot(gs[i, 2])
-        spacer_2 = fig.add_subplot(gs[i, 3])
-        ax_class = fig.add_subplot(gs[i, 4])
-
-        spacer_1.axis("off")
-        spacer_2.axis("off")
-
-        df["Dimension 1"] = X_2d[:, 0]
-        df["Dimension 2"] = X_2d[:, 1]
-
-        """
-        Color by GC-content
-        """
-        gc_values = df["GC"]
-        sc_gc = ax_gc.scatter(
-            X_2d[:, 0], X_2d[:, 1],
-            c=gc_values,
-            cmap="viridis", 
-            s=0.1
-        )
-
-        cax_gc = inset_axes(
-            ax_gc,
-            width="5%",
-            height="100%",
-            loc='lower left',
-            bbox_to_anchor=(1.01, 0.0, 1, 1),
-            bbox_transform=ax_gc.transAxes,
-            borderpad=0
-        )
-
-        cbar = plt.colorbar(sc_gc, ax=ax_gc, cax=cax_gc)
-        cbar.set_label('')
-
-        ax_gc.set_yticks([])
-        ax_gc.set_ylabel("")
-        if i == 0:
-            ax_gc.set_title("t-SNE Colored by GC Content")
-        if i < len(embedding_files) - 1:
-            ax_gc.set_xticks([])
-            ax_gc.set_xlabel("")
-        if i == len(embedding_files) - 1:
-            ax_gc.set_xlabel("Dimension 1")
-
-        """
-        Color by Region Length
-        """
-
-        reg_len = df["reg_len"]
-        sc_len = ax_len.scatter(
-            X_2d[:, 0], X_2d[:, 1],
-            c=reg_len,
-            cmap="viridis", 
-            s=0.1
-        )
-
-        cax_len = inset_axes(
-            ax_len,
-            width="5%",
-            height="100%",
-            loc='lower left',
-            bbox_to_anchor=(1.01, 0.0, 1, 1),
-            bbox_transform=ax_len.transAxes,
-            borderpad=0
-        )
-
-        cbar = plt.colorbar(sc_len, ax=ax_len, cax=cax_len)
-        cbar.set_label('')
-
-        ax_len.set_ylabel("Dimension 2")
-        if i == 0:
-            ax_len.set_title("t-SNE Colored by Region Length")
-        if i < len(embedding_files) - 1:
-            ax_len.set_xticks([])
-            ax_len.set_xlabel("")
-        if i == len(embedding_files) - 1:
-            ax_len.set_xlabel("Dimension 1")
-
-        """
-        Region Classification
-        """
-
-        class_colormap = 'tab10'
-        label_order = ["3UTR", "CDS", "intron", "intergenic", "5UTR"]
-        sns.scatterplot(
-            data=df,
-            x="Dimension 1",
-            y="Dimension 2",
-            hue="label",
-            hue_order=label_order,
-            s=5,
-            ax=ax_class,
-            palette=sns.color_palette(class_colormap, n_colors=len(label_order)),
-            legend=(i == len(embedding_files) - 1)
-        )
-
-        if i == len(embedding_files) - 1:
-            handles, labels = ax_class.get_legend_handles_labels()
-            ax_class.legend(
-                handles, labels,
-                loc='lower left',
-                frameon=False,
-                fontsize=10,
-                handlelength=1.5,
-                labelspacing=0.2,
-                markerscale=2.0
-            )
-
-        ax_class.set_ylabel("")
-        ax_class.set_yticks([])
-        if i == 0:
-            ax_class.set_title("t-SNE Colored by Genomic Region")
-        if i < len(embedding_files) - 1:
-            ax_class.set_xticks([])
-            ax_class.set_xlabel("")
-        if i == len(embedding_files) - 1:
-            ax_class.set_xlabel("Dimension 1")
-
-        ax_class.text(1.01, 0.5, f"Layer {layer + 1}", transform=ax_class.transAxes,
-                      rotation=270, va='center', ha='left', fontsize=12)
-
-        rect = patches.FancyBboxPatch(
-            (1.00, -0.002),
-            0.06, 1.0,
-            transform=ax_class.transAxes,
-            boxstyle="round,pad=0.00",
-            linewidth=0.5,
-            edgecolor='black',
-            facecolor='white',
-            clip_on=False
-        )
-        ax_class.add_patch(rect)
-
-    plt.subplots_adjust(hspace=0.02, bottom=0.04, top=0.93, left=0.05, right=0.98)
-    plt.suptitle(f"Genomic Region Determination - {model_name}", fontsize=16)
-
-    os.makedirs(class_dir, exist_ok=True)
-    plt.savefig(figure_path)
-    plt.show()
-
-import torch.nn.functional as F
 
 warnings.filterwarnings(
     "ignore",
@@ -246,7 +64,7 @@ def zero_shot_prediction(train_embeddings, train_labels, test_embeddings, test_l
     recall_per_class = {}
     for idx, cls in enumerate(classes):
         scores = sims[:, idx]
-        precision, recall, _ = precision_recall_curve(Y_test_bin[:, idx], scores)
+        precision, recall, _ = precision_recall_curve(Y_test_bin[:, idx], scores, drop_intermediate=False)
         ap = average_precision_score(Y_test_bin[:, idx], scores)
         ap_per_class[cls] = ap
         precision_per_class[cls] = precision
@@ -255,7 +73,7 @@ def zero_shot_prediction(train_embeddings, train_labels, test_embeddings, test_l
     return ap_per_class, precision_per_class, recall_per_class, classes
 
 def few_shot_prediction(train_embeddings, train_labels, test_embeddings, test_labels):
-    model = KNeighborsClassifier(n_neighbors=5, metric='cosine')
+    model = LogisticRegression(max_iter=5_000, solver='liblinear')
     model.fit(train_embeddings, train_labels)
     y_prob = model.predict_proba(test_embeddings)
     classes = model.classes_
@@ -266,7 +84,7 @@ def few_shot_prediction(train_embeddings, train_labels, test_embeddings, test_la
 
     for idx, cls in enumerate(classes):
         scores = y_prob[:, idx]
-        precision, recall, _ =  precision_recall_curve(y_test_bin[:, idx], scores)
+        precision, recall, _ =  precision_recall_curve(y_test_bin[:, idx], scores, drop_intermediate=False)
         ap = average_precision_score(y_test_bin[:, idx], scores)
         ap_per_class[cls] = ap
         precision_per_class[cls] = precision
@@ -276,6 +94,7 @@ def few_shot_prediction(train_embeddings, train_labels, test_embeddings, test_la
 
 def evaluate_class_prediction(model_name, file_list):
     class_dir = os.path.join(images_dir, 'class_genomic_regions')
+    os.makedirs(class_dir, exist_ok=True)
     figure_path = os.path.join(class_dir, f"{model_name}.pdf")
     tsne_dir = os.path.join(results_dir, 'tSNE', 'genomic_regions_annotated', model_name)
 
@@ -314,7 +133,6 @@ def evaluate_class_prediction(model_name, file_list):
 
             few_shot_res = few_shot_prediction(train_embeddings_normalized, train_y_true, test_embeddings_normalized, test_y_true)
             ap_per_class_few_shot, precision_per_class_few_shot, recall_per_class_few_shot, classes_few_shot = few_shot_res
-            pprint(ap_per_class_zero_shot)
             intermediate_results = {
                 'zero_shot': {
                     'ap_per_class_zero_shot': ap_per_class_zero_shot,
@@ -350,72 +168,158 @@ def evaluate_class_prediction(model_name, file_list):
     padding = (ymax - ymin) * 0.1
     ymin, ymax = ymin - padding, ymax + padding
 
-    figsize = (18, 4 * len(results))
-    fig = plt.figure(figsize=figsize)
-    gs = gridspec.GridSpec(2, 2, width_ratios=[1, 1], wspace=0.05, hspace=0.05)
-
-
-
     last_layer_result = results[last_layer]
+    tsne_file = os.path.join(tsne_dir, f"layer_{last_layer}.pkl")
+    with open(tsne_file, "rb") as f:
+        X_2d = pickle.load(f)
+
+    _, _, _, test_meta = load_pkl(file_list[-1])
+    zero_shot_res = last_layer_result["zero_shot"]
+    few_shot_res = last_layer_result["few_shot"]
+    ap_per_class_zero_shot = zero_shot_res["ap_per_class_zero_shot"]
+    precision_per_class_zero_shot = zero_shot_res["precision_per_class_zero_shot"]
+    recall_per_class_zero_shot = zero_shot_res["recall_per_class_zero_shot"]
+
+    ap_per_class_few_shot = few_shot_res["ap_per_class_few_shot"]
+    precision_per_class_few_shot = few_shot_res["precision_per_class_few_shot"]
+    recall_per_class_few_shot = few_shot_res["recall_per_class_few_shot"]
+
+    figsize = (45, 8)
+    fig = plt.figure(figsize=figsize)
+    gs = gridspec.GridSpec(1, 7, width_ratios=[1, 1, 0.1, 1, 0.2, 1, 1], wspace=0.05, hspace=0.05)
+
+    ax_class = fig.add_subplot(gs[0])
+    ax_gc = fig.add_subplot(gs[1])
+    ax_length = fig.add_subplot(gs[3])
+    ax_auprc_zero = fig.add_subplot(gs[5])
+    ax_auprc_few = fig.add_subplot(gs[6])
+
+    spacer_1 = fig.add_subplot(gs[2])
+    spacer_2 = fig.add_subplot(gs[4])
+    spacer_1.axis('off')
+    spacer_2.axis('off')
+
+    df = pd.DataFrame(test_meta)
+    df["Dimension 1"] = X_2d[:, 0]
+    df["Dimension 2"] = X_2d[:, 1]
+
+    """
+    Plot Actual Classes
+    """
+    class_colormap = 'tab10'
+    label_order = ["3UTR", "CDS", "intron", "intergenic", "5UTR"]
+    sc = sns.scatterplot(
+        data=df,
+        x="Dimension 1",
+        y="Dimension 2",
+        hue="label",
+        hue_order=label_order,
+        s=5,
+        ax=ax_class,
+        palette=sns.color_palette(class_colormap, n_colors=len(label_order)),
+        legend=True
+    )
+    ax_class.set_title("Genomic Classes", fontsize=18)
+    ax_class.set_xlabel("")
+    ax_class.set_ylabel("Dimension 2", fontsize=14)
+    handles, labels = ax_class.get_legend_handles_labels()
+    ax_class.legend(handles=handles[0:], labels=labels[0:])
 
 
+    """
+    Plot By GC content
+    """
+    gc_values = df["GC"]
+    sc_gc = ax_gc.scatter(
+        X_2d[:, 0], X_2d[:, 1],
+        c=gc_values,
+        cmap="viridis",
+        s=0.1
+    )
 
-    for i, result in enumerate(results):
-        layer = result
+    cax_gc = inset_axes(
+        ax_gc,
+        width="5%",
+        height="100%",
+        loc='lower left',
+        bbox_to_anchor=(1.01, 0.0, 1, 1),
+        bbox_transform=ax_gc.transAxes,
+        borderpad=0
+    )
 
-        zero_shot_res = intermediate_results["zero_shot"]
-        few_shot_res = intermediate_results["few_shot"]
-        ap_per_class_zero_shot = np.array(zero_shot_res["ap_per_class_zero_shot"])
-        precision_per_class_zero_shot = np.array(zero_shot_res["precision_per_class_zero_shot"])
-        recall_per_class_zero_shot = np.array(zero_shot_res["recall_per_class_zero_shot"])
-        classes_zero_shot = np.array(zero_shot_res["classes_zero_shot"])
+    cbar = plt.colorbar(sc_gc, ax=ax_gc, cax=cax_gc)
+    cbar.set_label('')
 
-        ap_per_class_few_shot = np.array(few_shot_res["ap_per_class_few_shot"])
-        precision_per_class_few_shot = np.array(few_shot_res["precision_per_class_few_shot"])
-        recall_per_class_few_shot = np.array(few_shot_res["recall_per_class_few_shot"])
-        classes_few_shot = np.array(few_shot_res["classes_few_shot"])
+    ax_gc.set_yticks([])
+    ax_gc.set_ylabel("")
+    ax_gc.set_title("Colored by GC content", fontsize=18)
+    ax_gc.set_xlabel("Dimension 1", fontsize=14)
 
-        tsne_file = os.path.join(tsne_dir, f"layer_{layer}.pkl")
-        with open(tsne_file, "rb") as f:
-            X_2d = pickle.load(f)
+    """
+    Plot By Sequence Length
+    """
+    reg_len = df["reg_len"]
+    sc_len = ax_length.scatter(
+        X_2d[:, 0], X_2d[:, 1],
+        c=reg_len,
+        cmap="viridis",
+        s=0.1
+    )
 
-        pprint(X_2d)
-        exit()
-        ax_auprc = fig.add_subplot(gs[i])
-        ax_auprc.set_ylim(ymin, ymax)
-        auprc_cmap = plt.cm.get_cmap(COLORMAP)
-        ax_auprc.plot(recall_zero_shot, precision_zero_shot, color=auprc_cmap(0.3), lw=2,
-                      label=f"auPRC Zero-Shot= {ap_zero_shot:.3f}")
-        ax_auprc.plot(recall_few_shot, precision_few_shot, color=auprc_cmap(0.6), lw=2,
-                      label=f"auPRC Regression= {ap_few_shot:.3f}")
+    cax_len = inset_axes(
+        ax_length,
+        width="5%",
+        height="100%",
+        loc='lower left',
+        bbox_to_anchor=(1.01, 0.0, 1, 1),
+        bbox_transform=ax_length.transAxes,
+        borderpad=0
+    )
 
-        if i == 0:
-            ax_auprc.set_ylabel("Precision", fontsize=14)
-        else:
-            ax_auprc.set_yticklabels([])
+    cbar = plt.colorbar(sc_len, ax=ax_length, cax=cax_len)
+    cbar.set_label('')
+    ax_length.set_yticks([])
+    ax_length.set_ylabel("")
+    ax_length.set_title("Colored by Region Length", fontsize=18)
+    ax_length.set_xlabel(""
+                         )
+    """
+    Plot Class auPRC
+    """
+    for c in label_order:
+        ax_auprc_zero.plot(recall_per_class_zero_shot[c], precision_per_class_zero_shot[c], lw=1,
+                  label=f"auPRC {c} = {ap_per_class_zero_shot[c]:.3f}")
 
-        ax_auprc.tick_params(left=False, bottom=False)
+    ax_auprc_zero.set_ylabel("Precision", fontsize=14)
+    ax_auprc_zero.set_title("Zero-Shot auPRC", fontsize=18)
+    ax_auprc_zero.set_xlabel("")
+    ax_auprc_zero.grid()
+    ax_auprc_zero.legend()
 
-        rect = patches.FancyBboxPatch(
-            (0.0, 1.0),
-            1.0, 0.12,
-            boxstyle="round,pad=0.00",
-            transform=ax_auprc.transAxes,
-            linewidth=0.5,
-            edgecolor='black',
-            facecolor='white',
-            clip_on=False
-        )
-        ax_auprc.text(
-            0.5, 1.05, f"Layer {int(layer) + 1}",
-            transform=ax_auprc.transAxes,
-            ha='center',
-            va='center',
-            fontsize=16
-        )
-        ax_auprc.add_patch(rect)
-        ax_auprc.legend()
-        ax_auprc.grid(True)
+    for c in label_order:
+        ax_auprc_few.plot(recall_per_class_few_shot[c], precision_per_class_few_shot[c], lw=1,
+                  label=f"auPRC {c} = {ap_per_class_few_shot[c]:.3f}")
+
+    ax_auprc_few.set_ylabel("")
+    ax_auprc_few.set_title("Regressive auPRC", fontsize=18)
+    ax_auprc_few.set_xlabel("")
+    ax_auprc_few.set_yticklabels([])
+    ax_auprc_few.tick_params(left=False, bottom=False)
+    ax_auprc_few.legend()
+    ax_auprc_few.grid()
+    fig.supxlabel("Recall",
+                  x=0.812,
+                  y=0.045,
+                  fontsize=14)
+    fig.subplots_adjust(
+        left=0.02,
+        right=0.99,
+        bottom=0.10,
+        top=0.95,
+    )
+    plt.show()
+    plt.savefig(figure_path)
+    plt.close(fig)
 
 if __name__ == "__main__":
     embeddings_folder = os.path.join(results_dir, 'embeddings', 'genomic_regions_annotated')
